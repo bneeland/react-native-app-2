8cbb98e08de87e2284234d87b47852ecd9a674c5
Brian Neeland <brian.neeland@protonmail.com>
2022-01-06 15:51:18 -0700

    Getting name text state updated to console

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredName, setEnteredName] = useState('');
+  // function nameInputHandler(enteredText) {
+  const nameInputHandler = (enteredText) => {
+    setEnteredName(enteredText)
+  };
+  const addNameHandler = () => {
+    console.log(enteredName);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter name..."
+          style={styles.input}
+          onChangeText={nameInputHandler}
+          value={enteredName}
+        />
+        <Button title="Add" onPress={addNameHandler} />
+      </View>
+      <View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
 });
